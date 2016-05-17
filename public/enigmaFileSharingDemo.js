fe4a2ea214987608655821c53db87450d0a98de9
Dusan Klinec <dusan.klinec@gmail.com>
2016-05-17 21:15:45 +0200

    Separating common demo features to the separate JS file.

---
+++ public/enigmaFileSharingDemo.js
@@ -0,0 +1,13 @@
+"use strict";
+var defaults = {
+    site: 'site2.enigmabridge.com',
+    site1: 'site1.enigmabridge.com',
+    site2: 'site2.enigmabridge.com'
+};
+
+// configuration
+var shareConfig = {
+    baseUrl: 'https://expert.enigmabridge.com/sharing',
+    shareFolderName: 'EnigmaShares',
+    clientId: '1044449456843-q4lt3nk61gulb67irbr45jvcr2siqfks.apps.googleusercontent.com'
+};
