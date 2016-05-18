2ba357c20a591e30bdae552986fee443f1bb0be8
Dusan Klinec <dusan.klinec@gmail.com>
2016-05-18 15:21:20 +0200

    xor8 function added
    - XORing 256bits, AES-256 keys, SHA256 results

--- public/enigma.js
+++ public/enigma.js
@@ -274,6 +274,9 @@ eb.misc = {
     xor: function(x,y){
         return [x[0]^y[0],x[1]^y[1],x[2]^y[2],x[3]^y[3]];
     },
+    xor8: function(x,y){
+        return [x[0]^y[0],x[1]^y[1],x[2]^y[2],x[3]^y[3],x[4]^y[4],x[5]^y[5],x[6]^y[6],x[7]^y[7]];
+    },
     absorb: function(dst, src){
         if (src === undefined){
             return dst;
