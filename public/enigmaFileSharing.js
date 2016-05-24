3e094be8bd5e7cedb6bfee6afaa99077e207202b
Dusan Klinec <dusan.klinec@gmail.com>
2016-05-24 10:23:39 +0200

    RetryHandler: tuned version, options, events

--- public/enigmaFileSharing.js
+++ public/enigmaFileSharing.js
@@ -103,13 +103,25 @@ eb.sh.misc = {
 
 /**
  * Helper for implementing retries with backoff. Initial retry
- * delay is 1 second, increasing by 2x (+jitter) for subsequent retries
- *
+ * delay is 250ms, increasing by 2x (+jitter) for subsequent retries
+ * @param {Object} [options]
+ * @param {Number} [options.startInterval] start interval of the backoff. The initial value. Default 250ms.
+ * @param {Number} [options.maxInterval] maximum waiting time, after reaching this point, backoff is not increased.
+ * @param {Number} [options.maxAttempts] maximum number of attempts before failing. -1 by default = do not fail
+ * @param {Number} [options.onFail] callback to call when maximum number of attempts was reached.
  * @constructor
  */
-var RetryHandler = function() {
-    this.interval = 1000; // Start at one second
-    this.maxInterval = 60 * 1000; // Don't wait longer than a minute
+var RetryHandler = function(options) {
+    var nop = function(){};
+    options = options || {};
+    this.startInterval = options.startInterval || 250; // Start at 250ms - quick retry
+    this.interval      = this.startInterval;
+    this.maxInterval = options.maxInterval || 60 * 1000; // Don't wait longer than a minute
+    this.maxAttempts = options.maxAttempts || -1;
+    this.onFail = options.onFail || nop;
+    this.logger = options.logger || nop;
+    this.curAttempts = 0;
+    this.curTimer = undefined;  // current setTimeout timer value. For cancellation.
 };
 
 /**
@@ -118,15 +130,54 @@ var RetryHandler = function() {
  * @param {function} fn Function to invoke
  */
 RetryHandler.prototype.retry = function(fn) {
-    setTimeout(fn, this.interval);
+    if (this.limitReached()){
+        this.logger("Retry: max number reached");
+        this.onFail(this);
+        return;
+    }
+
+    var curInterval = this.interval;
+    this.curTimer = setTimeout(fn, curInterval);
+    this.logger("Retry: next attempt in: " + curInterval);
+
     this.interval = this.nextInterval_();
+    this.curAttempts += 1;
+    return curInterval;
+};
+
+/**
+ * Cancels currently waiting back-off.
+ */
+RetryHandler.prototype.cancel = function() {
+    if (this.curTimer) {
+        clearTimeout(this.curTimer);
+        this.curTimer = undefined;
+    }
 };
 
 /**
  * Reset the counter (e.g. after successful request.)
  */
 RetryHandler.prototype.reset = function() {
-    this.interval = 1000;
+    this.cancel();
+    this.interval = this.startInterval;
+    this.curAttempts = 0;
+};
+
+/**
+ * Returns number of attempts.
+ * @returns {number}
+ */
+RetryHandler.prototype.numAttempts = function(){
+    return this.curAttempts;
+};
+
+/**
+ * Returns true if the limit on maximum number of attempts was reached.
+ * @returns {boolean}
+ */
+RetryHandler.prototype.limitReached = function(){
+    return this.maxAttempts >= 0 && this.maxAttempts <= this.curAttempts;
 };
 
 /**
