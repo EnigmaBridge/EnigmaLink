13f7d9bec2be8971a9da0fab20781b73b29b9159
Dusan Klinec <dusan.klinec@gmail.com>
2016-06-07 22:46:28 +0200

    blocs dependencies added

--- public/js/blocs.js
+++ public/js/blocs.js
@@ -1,49 +1,62 @@
-// Loading page complete
-$(window).load(function()
-{
-	checkHero(); // Check hero height is correct
-	animateWhenVisible();  // Activate animation when visible	
-});
-
 // Page ready
 $(document).ready(function()
 {
-	$('.hero').css('height', $(window).height()+'px'); // Set initial hero height
-	$('#scroll-hero').click(function()
+	$('.bloc-fill-screen').css('height', $(window).height()+'px'); // Set initial hero height
+	$('#scroll-hero').click(function(event)
 	{
-		$('html,body').animate({scrollTop: $("#hero-bloc").height()}, 'slow');
+		event.preventDefault();
+		$('html,body').animate({scrollTop: $('#scroll-hero').closest('.bloc').height()}, 'slow');
 	});
 	
+	setUpDropdownSubs(); // Set Up Dropdown Menu Support
 	setUpLightBox(); // Add lightbox Support
 });
 
-// Window resize 
-$(window).resize(function()
+// Loading page complete
+$(window).load(function()
+{
+	setFillScreenBlocHeight();
+	animateWhenVisible();  // Activate animation when visible	
+}
+).resize(function() // Window resize 
 {		
-	$('.hero').css('height',getHeroHeight()+'px'); // Refresh hero height  	
+	setFillScreenBlocHeight();	
 }); 
- 
-// Get Hero Height
-function getHeroHeight()
+
+// Set Fill Screen Bloc heights
+function setFillScreenBlocHeight()
+{
+	$('.bloc-fill-screen').each(function(i) // Loop all fill Screens
+	{
+		var parentFillDiv = $(this);
+		window.fillBodyHeight = 0;
+		$(this).find('.container').each(function(i) // Loop all fill Screens
+		{
+			fillPadding = parseInt($(this).css('padding-top'))*2
+			
+			if(parentFillDiv.hasClass('bloc-group')) // Bloc Groups
+			{
+				fillBodyHeight = fillPadding + $(this).outerHeight()+50; // Set hero body height
+			}
+			else
+			{
+				fillBodyHeight = fillBodyHeight + fillPadding + $(this).outerHeight()+50; // Set hero body height
+			}
+		});
+		$(this).css('height', (getFillHeight()) + 'px'); // Set Fill height
+	});
+}
+
+// Get Fill Height
+function getFillHeight()
 {
 	var H = $(window).height(); // Window height
 	
-	if(H < heroBodyH) // If window height is less than content height
+	if(H < fillBodyHeight) // If window height is less than content height
 	{
-		H = heroBodyH+100;
+		H = fillBodyHeight+100;
 	}
 	return H
-}
-
-// Check hero height
-function checkHero()
-{
-	if($('#hero-bloc').length)
-	{
-		P = parseInt($('.hero-nav').css('padding-top'))*2
-		window.heroBodyH = $('.hero-nav').outerHeight()+P+$('.vc-content').outerHeight()+50; // Set hero body height
-		$('.hero').css('height', getHeroHeight() + 'px'); // Set hero to fill page height
-	}
 }
 
 // Scroll to target
@@ -90,14 +103,42 @@ function animateWhenVisible()
 	});		
 };
 
+// Set Up Dropdown Menu Support
+function setUpDropdownSubs()
+{
+	$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event)
+	{
+		event.preventDefault(); 
+		event.stopPropagation(); 
+		$(this).parent().siblings().removeClass('open');
+		$(this).parent().toggleClass('open');
+		
+		var targetMenu = $(this).parent().children('.dropdown-menu');
+		var leftVal = targetMenu.offset().left+targetMenu.width();
+		if(leftVal > $(window).width())
+		{
+			targetMenu.addClass('dropmenu-flow-right');
+		}
+	});
+}
+
 // Hide all animation elements
 function stickyNavToggle()
 {
 	var V = 0; // offset Value
 	var C = "sticky"; // Classes
 	
-	if($('.sticky-nav').parent().is('#hero-bloc')) // If nav is in hero animate in
+	if($('.sticky-nav').hasClass('fill-bloc-top-edge')) // If nav is in hero animate in
 	{
+		var heroBackgroundColor = $('.fill-bloc-top-edge.sticky-nav').parent().css('background-color');
+
+		if(heroBackgroundColor == "rgba(0, 0, 0, 0)")
+		{
+			heroBackgroundColor = '#FFFFFF'
+		}
+		
+		$('.sticky-nav').css('background', heroBackgroundColor); 
+		
 		V = $('.sticky-nav').height();
 		C = "sticky animated fadeInDown";
 	}
@@ -113,7 +154,7 @@ function stickyNavToggle()
 	}
 	else
 	{
-		$('.sticky-nav').removeClass(C);
+		$('.sticky-nav').removeClass(C).removeAttr('style');
 		$('.page-container').removeAttr('style');
 	}	
 }
