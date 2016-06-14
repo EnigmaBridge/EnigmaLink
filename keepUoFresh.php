f4d755c88597e7d20e6181ae7a4c16a4029e327f
Dusan Klinec <dusan.klinec@gmail.com>
2016-06-14 08:34:54 +0200

    keepUoFresh added

---
+++ keepUoFresh.php
@@ -0,0 +1,22 @@
+<?php
+$reqEnc = 'https://site1.enigmabridge.com:11180/1.0/TEST_API000000007d/ProcessData/0419b9a2fcae6edd/Packet0_PLAINAES_0000d4e4748b78444d4e2e3f5a51b03c4b087041e7b29a5924e65b5880c9886115a560332bf8e2d4064ca37e273fd0ba4a06';
+$reqDec = 'https://site1.enigmabridge.com:11180/1.0/TEST_API000000007f/ProcessData/7d555e4e2f13a2cb/Packet0_PLAINAESDECRYPT_00000c85a521509775382a1e9c59862312c36bd61a6d7e64f2269df05b6ee73a04729f9e2b6c0e646544c39452cac313cd1d';
+visit($reqEnc);
+visit($reqDec);
+visit($reqEnc);
+visit($reqDec);
+visit($reqEnc);
+visit($reqDec);
+
+
+function visit($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+    $response = curl_exec($ch);
+}
