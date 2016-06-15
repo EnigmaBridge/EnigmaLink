db20185eeaa09d5ebcabc25157e49d32c5f84fd8
Dusan Klinec <dusan.klinec@gmail.com>
2016-06-15 19:40:18 +0200

    experiment: attempt with custom blob type

--- public/js/FileSaver.js
+++ public/js/FileSaver.js
@@ -206,7 +206,7 @@ console.log("save link fired");
                 // Update: Google errantly closed 91158, I submitted it again:
                 // https://code.google.com/p/chromium/issues/detail?id=389642
 console.log("view.chrome: " + view.chrome);
-                if ((view.chrome || is_chrome_ios) && type && type !== force_saveable_type) {
+                if ((view.chrome ) && type && type !== force_saveable_type) {
 console.log("chrome & not forceable save type: ");
                     slice = blob.slice || blob.webkitSlice;
                     blob = slice.call(blob, 0, blob.size, force_saveable_type);
