bc2de1fe520a232f9f5d3ccc725259a10ab2b3fd
Dusan Klinec <dusan.klinec@gmail.com>
2016-06-15 19:33:42 +0200

    experiment: filesaver 2

--- public/js/FileSaver.js
+++ public/js/FileSaver.js
@@ -151,7 +151,7 @@ console.log("opening object url");
 console.log("opening new tab on object url");
                             var new_tab = view.open(object_url, "_blank");
 console.log("new tab opened:");
-console.log(new_tab);
+//console.log(new_tab);
 //                            if (new_tab !== undefined && new_tab.location && new_tab.location.href === "about:blank" && is_chrome_ios){
 //console.log("open failed2");
 //                                view.location.href = object_url
