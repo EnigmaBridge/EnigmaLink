cda1854e73d308afc44253952377805c2e460466
Dusan Klinec <dusan.klinec@gmail.com>
2016-06-21 10:34:57 +0200

    EL-37: debugging proxy mode - remove

--- public/js/downloader.js
+++ public/js/downloader.js
@@ -275,6 +275,7 @@ function downloadClicked() {
         encScheme: encScheme,
         chunkSizeAdaptive: true,
         chunkSizeMax: jQuery.browser.mobile ? 1024*1024 : 1024*1024*4, // 4MB chunk size
+        downloadAllAtOnceLimit: 1024*1024, // TODO: remove!!!
         rangeNotAllowed: isRangeCorsProblemPossible() || true, // TODO: REMOVE!!!
         onProgress: function(oEvent, aux){
             if (oEvent && oEvent.lengthComputable && oEvent.loaded && aux && aux.offset && aux.total) {
